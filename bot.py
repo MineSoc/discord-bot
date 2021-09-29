5db4a8f50a727b77a4ec412ec8e42983e2fcbbe2
ericthelemur <owenconnors1@gmail.com>
2021-09-29 18:20:36 +0100

    Added reaction role capability

--- bot.py
+++ bot.py
@@ -1,10 +1,17 @@
 import os
+from typing import Union, Optional
+import re
 
 import discord
+import discord_components
+from discord.abc import Messageable
 from discord.ext.commands import when_mentioned_or, CommandNotFound, has_permissions, has_role, NoPrivateMessage, Bot, \
-    ExpectedClosingQuoteError
+    ExpectedClosingQuoteError, Context
 
-from discord_components import DiscordComponents, ComponentsBot, Button
+from discord_components import DiscordComponents, ComponentsBot, Button, ActionRow
+import json
+
+from proceffects import *
 
 TOKEN = os.getenv('WMCS_DISCORD_TOKEN')
 
@@ -13,7 +20,12 @@ def get_prefix(bot, message: discord.Message):
     return when_mentioned_or("WMCS!")(bot, message)
 
 
-bot = ComponentsBot(command_prefix=get_prefix)
+intents = discord.Intents.default()
+intents.reactions = True
+intents.members = True
+
+bot = ComponentsBot(command_prefix=get_prefix, intents=intents)
+
 
 @bot.event
 async def on_ready():
@@ -23,19 +35,47 @@ async def on_ready():
 @has_role("Exec")
 @bot.command(name="mcguide")
 async def mcguide(ctx):
-    await ctx.send(
-        "**Server Guides**",
-        components=[
-            [
-                Button(label="Survival", style=5, url="https://discord.com/channels/633724840330788865/882024666762457118/882024833813188649")
-            ],
-            [
-                Button(label="UHC", style=5, url="https://discord.com/channels/633724840330788865/882025373179731978/882025374463168522"),
-                Button(label="RftW", style=5, url="https://discord.com/channels/633724840330788865/882025787690201148/882025789346947082"),
-                Button(label="Bedwars", style=5, url="https://discord.com/channels/633724840330788865/882025941197520926/882025942350979084")
-            ]
-        ],
-    )
+    print(bot.emojis)
+    print(bot.get_guild(633724840330788865).channels)
+    guide_components = [ActionRow.from_json(b) for b in (json.loads("""[
+    {"type": 1, "components": [
+        {"type": 2, "style": 5, "disabled": "false", "label": "Survival", "url": "https://discord.com/channels/633724840330788865/882024666762457118/882024833813188649", "emoji": {"name": "pick", "id": 882526831566291004}}, 
+        {"type": 2, "style": 5, "label": "Bedrock", "url": "https://discord.com/channels/633724840330788865/884842296112214077/884842297613774848", "emoji": {"name": "bedrockblock", "id": 884818408036765756}}
+    ]}, {"type": 1, "components": [
+        {"type": 2, "style": 5, "label": "UHC", "url": "https://discord.com/channels/633724840330788865/882025373179731978/882025374463168522", "emoji": {"name": "apple", "id": 651235805951557633}}, 
+        {"type": 2, "style": 5, "label": "RftW", "url": "https://discord.com/channels/633724840330788865/882025787690201148/882025789346947082", "emoji": {"name": "wool", "id": 884818265598201917}}, 
+        {"type": 2, "style": 5, "label": "Bedwars", "url": "https://discord.com/channels/633724840330788865/882025941197520926/882025942350979084", "emoji": {"name": "bed", "id": 884816256077824042}}
+
+    ]}]"""))]
+
+    await ctx.send("""_ _
+<:sqwave2:880600411009060874> **WELCOME TO WARWICK MINECRAFT SOCIETY!** <:sqwave:655468091341406233>
+We run both a survival server and frequent events:
+Our **survival server** has recently moved to a new spawn! Read more in the link below.
+Connect with `warwickmc.uk`, *or `proxy.warwickmc.uk` if on campus.*
+
+Over the summer, we are running **fortnightly events on Thursdays at 7:30pm**. These typically consist of (speed) UHCs and a variety of minigames - with more larger gamemodes coming soon! Read about the gamemodes in use above!
+
+Everything is currently running on Java 1.17.1. Bedrock crossplay/servers is partially implemented, though dedicated bedrock servers are unlikely. Get the role in <#692132927068307486> to be notified of Bedrock news!
+
+Check <#717026685383475330> for the server rules and the links below for information on our servers and <#699591152294297621> for our current execs!
+Get your pronoun and fresher roles in <#692132927068307486> and introduce yourself in <#877927457880162304>!
+
+**Next event: Hypixel minigames on Thursday 23rd Sept at 7:30pm**
+_ _
+**Server Guides**""", components=guide_components)
+
+
+@has_role("Exec")
+@bot.command(name="msg", help="Posts a blank message by this bot, to edit")
+async def msg(ctx):
+    await ctx.send("_ _")
+
+
+def get_gcm(url: str):
+    # https://discord.com/channels/633724840330788865/867386809688391720/876095725816135710
+    g = re.search(r"(https?://)?(www.)?discord.com/channels/(\d+)/(\d+)/(\d+)", url)
+    return int(g.group(3)), int(g.group(4)), int(g.group(5))
 
 
 @bot.event
@@ -46,7 +86,8 @@ async def on_error(ctx, err, *args, **kwargs):
 
 
 @bot.event
-async def on_command_error(ctx, error):
+async def on_command_error(ctx, error: Exception):
+    await ctx.message.add_reaction("🚫")
     if isinstance(error, CommandNotFound):
         pass
     elif isinstance(error, NoPrivateMessage):
@@ -54,7 +95,13 @@ async def on_command_error(ctx, error):
     elif isinstance(error, ExpectedClosingQuoteError):
         await ctx.send(f"Mismatching quotes, {str(error)}")
     elif hasattr(error, "original"):
+        await ctx.send(f"{error}")
         raise error.original
-    else: raise error
+    else:
+        await ctx.send(f"{error}")
+        raise error
 
-bot.run(TOKEN)
+
+if __name__ == '__main__':
+    bot.load_extension("reaction_roles")
+    bot.run(TOKEN)
---
+++ reaction_roles.py
@@ -0,0 +1,177 @@
+import discord
+from discord.embeds import EmbedProxy
+from discord.ext import commands
+import json
+import atexit
+import uuid
+
+from discord_components import ComponentsBot
+
+reaction_roles_data = {}
+
+# Load or create storage JSON
+try:
+    with open("reaction_roles.json") as file:
+        reaction_roles_data = json.load(file)
+except (FileNotFoundError, json.JSONDecodeError) as ex:
+    with open("reaction_roles.json", "w") as file:
+        json.dump({}, file)
+
+
+# Store unsaved data on exit
+@atexit.register
+def store_reaction_roles():
+    with open("reaction_roles.json", "w") as file:
+        json.dump(reaction_roles_data, file)
+
+
+class ReactionRoles(commands.Cog):
+    bot: ComponentsBot
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f"ReactionRoles ready.")
+
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
+        if payload.user_id == self.bot.user.id: return
+        # Get role from reaction (if exists)
+        role, user = self.parse_reaction_payload(payload)
+
+        if role is not None and user is not None:
+            # Toggle role
+            if role not in user.roles: await user.add_roles(role, reason="ReactionRole")
+            else: await user.remove_roles(role, reason="ReactionRole")
+
+            # Remove reaction
+            channel: discord.TextChannel = self.bot.get_channel(payload.channel_id)
+            message: discord.PartialMessage = channel.get_partial_message(payload.message_id)
+            await message.remove_reaction(payload.emoji, user)
+
+
+    @commands.has_permissions(manage_channels=True)
+    @commands.command(help="Create message to add reactions to")
+    async def reaction(self, ctx, channel: discord.TextChannel, title, message):
+        embed = discord.Embed(title=title, description=message)
+        embed.add_field(name="Roles", value="_ _", inline=False)
+        await channel.send(embed=embed)
+
+    @commands.has_permissions(manage_channels=True)
+    @commands.command(help="Add reaction to reaction message")
+    async def reaction_add(self, ctx, channel: discord.TextChannel, message_id, emote, role: discord.Role, description=None):
+        message: discord.Message = await channel.fetch_message(message_id)
+        if message.author == self.bot:
+            embed: discord.Embed = message.embeds[0]
+            field: EmbedProxy = embed.fields[0]
+            embed.remove_field(0)
+
+            if field.value == "_ _": field.value = ""
+            new_val = field.value + f"\n{emote} {description if description is not None else role.name}"
+            embed.add_field(name=field.name, value=new_val)
+            await message.edit(embed=embed)
+
+        await message.add_reaction(emote)
+        self.add_reaction(ctx.guild.id, emote, role.id, channel.id, message_id)
+
+
+
+    @commands.has_permissions(manage_channels=True)
+    @commands.command()
+    async def reactions(self, ctx):
+        guild_id = ctx.guild.id
+        data = reaction_roles_data.get(str(guild_id), None)
+        embed = discord.Embed(title="Reaction Roles")
+        if data is None:
+            embed.description = "There are no reaction roles set up right now."
+        else:
+            for index, rr in enumerate(data):
+                emote = rr.get("emote")
+                role_id = rr.get("roleID")
+                role = ctx.guild.get_role(role_id)
+                channel_id = rr.get("channelID")
+                message_id = rr.get("messageID")
+                embed.add_field(
+                    name=str(index),
+                    value=f"{emote} - @{role} - [message](https://www.discordapp.com/channels/{guild_id}/{channel_id}/{message_id})",
+                    inline=False,
+                )
+        await ctx.send(embed=embed)
+
+
+    @commands.has_permissions(manage_channels=True)
+    @commands.command()
+    async def reaction_remove(self, ctx, index: int):
+        guild_id = ctx.guild.id
+        data = reaction_roles_data.get(str(guild_id), None)
+        embed = discord.Embed(title=f"Remove Reaction Role {index}")
+        rr = None
+        if data is None:
+            embed.description = "Given Reaction Role was not found."
+        else:
+            embed.description = (
+                "Do you wish to remove the reaction role below? Please react with 🗑️."
+            )
+            rr = data[index]
+            emote = rr.get("emote")
+            role_id = rr.get("roleID")
+            role = ctx.guild.get_role(role_id)
+            channel_id = rr.get("channelID")
+            message_id = rr.get("messageID")
+            _id = rr.get("id")
+            embed.set_footer(text=_id)
+            embed.add_field(
+                name=str(index),
+                value=f"{emote} - @{role} - [message](https://www.discordapp.com/channels/{guild_id}/{channel_id}/{message_id})",
+                inline=False,
+            )
+        msg = await ctx.send(embed=embed)
+        if rr is not None:
+            await msg.add_reaction("🗑️")
+
+            def check(reaction, user):
+                return (
+                    reaction.message.id == msg.id
+                    and user == ctx.message.author
+                    and str(reaction.emoji) == "🗑️"
+                )
+
+            reaction, user = await self.bot.wait_for("reaction_add", check=check)
+            data.remove(rr)
+            reaction_roles_data[str(guild_id)] = data
+            store_reaction_roles()
+
+    def add_reaction(self, guild_id, emote, role_id, channel_id, message_id):
+        if not str(guild_id) in reaction_roles_data:
+            reaction_roles_data[str(guild_id)] = []
+        reaction_roles_data[str(guild_id)].append(
+            {
+                "id": str(uuid.uuid4()),
+                "emote": emote,
+                "roleID": role_id,
+                "channelID": channel_id,
+                "messageID": int(message_id),
+            }
+        )
+        store_reaction_roles()
+
+    def parse_reaction_payload(self, payload: discord.RawReactionActionEvent):
+        guild_id = payload.guild_id
+        data = reaction_roles_data.get(str(guild_id), None)
+        print(payload)
+        print(data)
+        if data is not None:
+            for rr in data:
+                if payload.message_id == rr.get("messageID") and payload.channel_id == rr.get("channelID") and str(payload.emoji) == rr.get("emote"):
+                    guild = self.bot.get_guild(guild_id)
+                    role = guild.get_role(rr.get("roleID"))
+                    user = guild.get_member(payload.user_id)
+                    return role, user
+        return None, None
+
+
+def setup(bot):
+    bot.add_cog(ReactionRoles(bot))
